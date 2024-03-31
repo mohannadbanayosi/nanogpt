7bbe640a3f3877befc893904362227dd55f89700
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-03-31 20:22:09 +0200

    add: feed forward layer

--- biagram.py
+++ biagram.py
@@ -105,6 +105,16 @@ class Head(nn.Module):
 
         return out
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd) -> None:
+        super().__init__()
+        self.net = nn.Sequential(nn.Linear(n_embd, n_embd), nn.ReLU())
+    
+    def forward(self, x):
+        return self.net(x)
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
@@ -113,7 +123,7 @@ class BigramLanguageModel(nn.Module):
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         # self.sa_head = Head(n_embd)
         self.sa_heads = MultiHeadAttention(4, n_embd//4) # ie 4 heads of 8 dimensional self attention
-
+        self.ffwd = FeedForward(n_embd)
         # TODO: check why linear model
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
@@ -125,6 +135,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb  # (B,T,C)
         x = self.sa_heads(x)  # apply one head of self attention
+        x = self.ffwd(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
