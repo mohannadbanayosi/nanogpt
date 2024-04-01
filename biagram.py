597f94d45ad7489109fea0f5ed62a504e2e58907
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-04-01 15:35:54 +0200

    update: default to inference mode

--- biagram.py
+++ biagram.py
@@ -8,7 +8,7 @@ batch_size = 16 # how many independent sequences will we process in parallel?
 block_size = 32 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-3  # TODO: understand this
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 128
@@ -239,4 +239,4 @@ def eval():
 
 
 if __name__ == "__main__":
-    train()
+    eval()
