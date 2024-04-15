45bfc05f2ad7eadef9c28274c8ff436a096ccb82
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-04-15 09:51:14 +0200

    refactor: move model to `model.py`

---
+++ model.py
@@ -0,0 +1,146 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+
+
+@dataclass
+class BLMConfig:
+    vocab_size: int = 10
+    n_embd: int = 192    
+    block_size: int = 128
+    n_head: int = 4
+    n_layer: int = 6
+    dropout: float = 0.2
+    device: str = "cpu"
+
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads self attention """
+
+    def __init__(self, config: BLMConfig, head_size) -> None:
+        super().__init__()
+        self.heads = nn.ModuleList(Head(config=config, head_size=head_size) for _ in range(config.n_head))
+        self.projection = nn.Linear(config.n_embd, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
+    
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.projection(out))
+        return out
+
+
+class Head(nn.Module):
+    """ one head self attention """
+
+    def __init__(self, config: BLMConfig, head_size) -> None:
+        super().__init__()
+
+        # TODO: move bias to config
+        self.key = nn.Linear(config.n_embd, head_size, bias=False)
+        self.query = nn.Linear(config.n_embd, head_size, bias=False)
+        self.value = nn.Linear(config.n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(config.block_size, config.block_size)))
+        self.dropout = nn.Dropout(config.dropout)
+    
+    def forward(self, x):
+        B,T,C = x.shape
+
+        k = self.key(x)   # (B,T,C)
+        q = self.query(x) # (B,T,C)
+
+        # compute affinities
+        wei =  q @ k.transpose(-2, -1) * C**-0.5  # (B, T, C) @ (B, C, T) ---> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) 
+        wei = nn.functional.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        # compute weighted aggregation
+        v = self.value(x)
+        out = wei @ v
+
+        return out
+
+
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, config: BLMConfig) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(config.n_embd, 4*config.n_embd),
+            nn.ReLU(),
+            nn.Linear(4*config.n_embd, config.n_embd),
+            nn.Dropout(config.dropout)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, config):
+        super().__init__()
+        head_size = config.n_embd // config.n_head
+        self.sa = MultiHeadAttention(config, head_size)
+        self.ffwd = FeedForward(config)
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+    
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class BigramLanguageModel(nn.Module):
+    """ core model """
+
+    def __init__(self, config: BLMConfig):
+        super().__init__()
+        self.config = config
+
+        self.token_embedding_table = nn.Embedding(config.vocab_size, config.n_embd)
+        self.position_embedding_table = nn.Embedding(config.block_size, config.n_embd)
+        self.blocks = nn.Sequential(*[Block(config=config) for _ in range(config.n_layer)])
+        self.ln_f = nn.LayerNorm(config.n_embd)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=self.config.device)) # (T,C)
+        x = tok_emb + pos_emb  # (B,T,C)
+        x = self.blocks(x)
+        x = self.ln_f(x)  # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = nn.functional.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx
+            idx_cropped = idx[:, -self.config.block_size:]
+            # get the predictions
+            logits, loss = self(idx_cropped)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = nn.functional.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
