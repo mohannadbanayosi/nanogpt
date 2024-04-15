7566db73e5a9ac9bd9d52b13211dd5b3b7905390
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-04-15 21:37:24 +0200

    refactor: remove comments

--- model.py
+++ model.py
@@ -129,18 +129,11 @@ class BigramLanguageModel(nn.Module):
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
-        # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
-            # crop idx
             idx_cropped = idx[:, -self.config.block_size:]
-            # get the predictions
             logits, loss = self(idx_cropped)
-            # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
-            # apply softmax to get probabilities
             probs = nn.functional.softmax(logits, dim=-1) # (B, C)
-            # sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-            # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
